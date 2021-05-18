a2d9423a890d4585f8a05ed0f635fe822038d049
Xnova98 <hoongquan98@gmail.com>
2021-05-18 23:12:18 +0800

    Update testindex.blade.php

--- resources/views/manageRepairStatus/testindex.blade.php
+++ resources/views/manageRepairStatus/testindex.blade.php
@@ -2,7 +2,10 @@
 <html>
 <body>
 
-
+<p>@if(session()->get('success'))
+      {{ session()->get('success') }}  
+<br></p>
+@endif
 <button onclick="location.href='{{ route('manageRepairStatus.index') }}'">
      Staff View Requested Repair</button>
      <button onclick="location.href='{{ route('manageRepairStatus.update', 20001)}}'">
