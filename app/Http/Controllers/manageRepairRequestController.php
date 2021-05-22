fcb89386bc214f644fa8baa6efcb00d7665dc709
vanesewong <vanesev7@gmail.com>
2021-05-22 23:59:35 +0800

    Update manageRepairRequestController.php

--- app/Http/Controllers/manageRepairRequestController.php
+++ app/Http/Controllers/manageRepairRequestController.php
@@ -10,18 +10,35 @@ class manageRepairRequestController extends Controller
     function requestdetail(Request $req){
         //print_r($detail->input());
         $detail = new manageRepairRequestModel;
+        $detail->Customer_ID= $req->custID;
+        $detail->Order_Status= $req->compOwner;
         $detail->Comp_Owner= $req->compOwner;
         $detail->Comp_Model = $req->compModel;
         $detail->Warranty_Date = $req->warrantyDate;
         $detail->Problems_Frequency = $req->problemsFrequency;
         $detail->Problems_Reported = $req->problemsReported;
+        $detail->Reason = $req->reason;
+        $detail->Estimated_Cost = $req->estimatedCost;
+        $detail->Confirmation_Status = $req->confirmationStatus;
         $detail->Send_Status = $req->sendStatus;
         $detail->save();
     }
 
     //VIEW
-    function list(){
+    /*function list(){
         $data = manageRepairRequestModel::all();
-        return view('manageRepairRequest.custView',compact("data"));
+        return view('manageRepairRequest.viewDraft',compact("data"));
+    }*/
+
+    //VIEW draft
+    public function list($id)
+    {
+        $data = manageRepairRequestModel::where('Customer_ID', $id)->get();
+        return view('manageRepairRequest.viewDraft', compact("data"));
     }
+    //EDIT for drafting
+    /*function editData($id){
+        $data = manageRepairRequestModel::findOrFail($id);
+        return view('manageRepairRequest.editDraft', compact("data"));
+    }*/
 }
