a970790af26ef8110bb827d94a1a5a481722fa31
qiupeng3110 <qiupeng103198@gmail.com>
2021-05-22 22:21:28 +0800

    Updated rider.php

--- app/Models/rider.php
+++ app/Models/rider.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class rider extends Model
 {
     use HasFactory;
+
+    protected $guard = 'rider';
+
+    protected $fillable =[
+        'Rider_Name', 'Rider_IC', 'Rider_Email', 'Rider_Phone', 'Rider_Address', 'Rider_IC_photo', 'Rider_Licence', 'Rider_Password',
+    ];
+
+    protected $hidden = [
+        'Rider_Password', 'remember_token',
+    ];
 }
