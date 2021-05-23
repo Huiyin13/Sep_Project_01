bd099b10e4ade0de8d1c5d8a18cb235d44a75c5f
qiupeng3110 <qiupeng103198@gmail.com>
2021-05-23 15:01:44 +0800

    Updated ManageRegistration.php
    Commented line 7

--- app/Http/Controllers/ManageRegistrationController.php
+++ app/Http/Controllers/ManageRegistrationController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 // Newly added controller 
-use App\Http\Controllers;
+// use App\Http\Controllers;
 use App\Providers\RouteServiceProvider;
 use App\Models\customer;
 use App\Models\rider;
