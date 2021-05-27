71175de173b8e40d2ea1781a0be6a9b350423834
Huiyin13 <83337412+Huiyin13@users.noreply.github.com>
2021-05-28 00:48:14 +0800

    Update ManageAccountController.php

--- app/Http/Controllers/ManageAccountController.php
+++ app/Http/Controllers/ManageAccountController.php
@@ -10,9 +10,12 @@ use App\Models\customer;
 use App\Models\rider;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+
 
 class ManageAccountController extends Controller
 {
+    //Customer
     public function selectProfile($id)
     {
         $data = customer::where('Customer_ID', $id)->get();
@@ -59,7 +62,8 @@ class ManageAccountController extends Controller
         foreach($data as $data1){
             $data2 = $data1->Customer_Password;
         }
-        if ( $validatedPass == $data2) {
+        $verify = password_verify($validatedPass,$data2);
+        if ( $verify) {
             
             $validatedData = $request->validate([
                 'Customer_Name' => 'required|max:255',
@@ -102,7 +106,7 @@ class ManageAccountController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function updatePassword(Request $request, $id)
+    public function changePass(Request $request, $id)
     {
         //
         $data = customer::where('Customer_ID', $id)->get();
@@ -110,7 +114,8 @@ class ManageAccountController extends Controller
         foreach($data as $data1){
             $data2 = $data1->Customer_Password;
         }
-        if ( $validatedPass == $data2) {
+        $verify = password_verify($validatedPass,$data2);
+        if ( $verify) {
             
             $validatedData = $request->validate([
                 'Customer_Password' => 'required|max:255',
@@ -130,6 +135,113 @@ class ManageAccountController extends Controller
         }
     }
 
+    //Rider
+    public function selectProfileR($id)
+    {
+        $data = rider::where('Rider_ID', $id)->get();
+        return view('ManageAccount.RiderProfileInterface', compact("data"));
+    }
+
+     /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function editProfileR($id)
+    {
+        //
+        $data = rider::where('Rider_ID', $id)->get();
+        return view('ManageAccount.RiderUpdateInterface', compact("data"));
+    }
+
     
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function updateR(Request $request, $id)
+    {
+        //
+        $data = rider::where('Rider_ID', $id)->get();
+        $validatedPass = $request->Rider_Password;
+        foreach($data as $data1){
+            $data2 = $data1->Rider_Password;
+        }
+        $verify = password_verify($validatedPass,$data2);
+        if ( $verify) {
+            
+            $validatedData = $request->validate([
+                'Rider_Name' => 'required|max:255',
+                'Rider_Phone' => 'required|max:255',
+                'Rider_Email' => 'required|max:255',
+                'Rider_Address' => 'required|max:255',
+
+            ]);
+            rider::where('Rider_ID', $id)->update($validatedData);
+            $message = "Profile is successful updated!";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+            return view('ManageAccount.test');
+        } 
+        else {
+            $data = rider::where('Rider_ID', $id)->get();
+             $message = "Password INCORRECT please try again";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+            return view('ManageAccount.RiderUpdateInterface', compact("data"));
+        }
+    }
+
+ /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function changePasswordR($id)
+    {
+        //
+        $data = rider::where('Rider_ID', $id)->get();
+        return view('ManageAccount.ChangePasswordInterface', compact("data"));
+        
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function changePassR(Request $request, $id)
+    {
+        //
+        $data = rider::where('Rider_ID', $id)->get();
+        $validatedPass = $request->Rider_Passwordo;
+        foreach($data as $data1){
+            $data2 = $data1->Rider_Password;
+        }
+       // $verify = password_verify($validatedPass,$data2);
+        if ( $validatedPass == $data2) {
+            
+            $validatedData = Hash::make($request->Rider_Password);
+            
+            rider::where('Rider_ID', $id)->update(explode(",", $validatedData));
+            $data = rider::where('Rider_ID', $id)->get();
+            $message = "Profile is successful updated!";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+            return view('ManageAccount.RiderProfileInterface', compact("data"));
+        } 
+        else {
+            $data = rider::where('Rider_ID', $id)->get();
+             $message = "Password INCORRECT please try again";
+            echo "<script type='text/javascript'>alert('$message');</script>";
+            return view('ManageAccount.ChangePasswordInterface', compact("data"));
+        }
+    }
+
 }
 
