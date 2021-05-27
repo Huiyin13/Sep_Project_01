1c4048b0d3aa20d0742ad50f20fe00d14fdbd5de
Huiyin13 <83337412+Huiyin13@users.noreply.github.com>
2021-05-28 00:48:08 +0800

    Update test.blade.php

--- resources/views/ManageAccount/test.blade.php
+++ resources/views/ManageAccount/test.blade.php
@@ -8,6 +8,6 @@
 @endif
 
      <button onclick="location.href='{{ route('ManageAccount.selectProfile', 1)}}'">Customer </button>
-     <button onclick="location.href='{{ route('ManageAccount.selectProfileR', 4)}}'">Rider </button>
+     <button onclick="location.href='{{ route('ManageAccount.selectProfileR', 3)}}'">Rider </button>
 </body>
 </html>
