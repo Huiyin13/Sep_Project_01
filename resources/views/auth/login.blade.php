9c1bd74ea1d82771aaf46222e0eac7317217975a
qiupeng3110 <qiupeng103198@gmail.com>
2021-05-29 01:13:07 +0800

    Updated login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,28 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
-
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
-
-                        <div class="form-group row">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
-
-                        <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
+<!DOCTYPE html> 
+<html>
+<head>
+	<title>Login Main Page</title>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+</head>
+<body>
+	<center> 
+		<h1><b>Login as: </b></h1>
+		<table>
+			<tr>
+				<td style="text-align: center;"><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQJs-9vUlbFLFKrhJpV2NNvKRUuZedoWALEf0dbAiK46nMv_aV8EG6ms4cyZoUXyGxmkcE&usqp=CAU" alt="Customer Logo" style="width: 150px; height: 150px;"></td>
+				<td style="text-align: center;"><img src="/images/Rider_Logo.png" alt="Rider Logo" style="width: 175px; height: 100px;"></td>
+				<td style="text-align: center;"><img src="https://cdn2.iconfinder.com/data/icons/web-solid/32/user-512.png" alt="Rider Logo" style="width: 100px; height: 100px;"></td>
+			</tr>
+			<tr>
+				<td style="text-align: center;"><button><a href="auth/CustLogin" style="color: white; text-decoration: none;">CUSTOMER</a></button></td>
+				<td style="text-align: center;"><button><a href="auth/RiderLogin" style="color: white; text-decoration: none;">RIDER</a></button></td>
+				<td style="text-align: center;"><button><a href="auth/StaffLogin" style="color: white; text-decoration: none;">STAFF</a></button></td>
+			</tr>
+		</table>
+	</center>
+</body>
+</html>
 @endsection
