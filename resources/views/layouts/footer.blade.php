b3a6fa7e51c5c9cc68c6e6d5447423a2ae020b54
qiupeng3110 <qiupeng103198@gmail.com>
2021-05-29 13:17:16 +0800

    Created and updated footer.blade.php

---
+++ resources/views/layouts/footer.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head> 
+    <title>Footer</title>
+</head>
+<body>
+    <div class="footer" style="background-color: #e6e6e6">
+    <hr>
+        <table>
+            <tr>
+                <td rowspan="4" style="text-align: center"><img src="/images/Logo.png" alt="Dercs Logo" /></td>
+                <th><i>Operating Hour</i></th>
+                <th><i>Contact Us</i></th>
+                <th><i>Location</i></th>
+                <td rowspan="4" style="text-align: center"><img src="/images/Map.png" style="width:200px; height:130px" alt="Dercs Map" /></td>
+            </tr>
+            <tr>
+                <td>8.00 A.M. TO 10.00 P.M. -Weekday-</td>
+                <td>+(60)14-8403374</td>
+                <td>44-34, Jalan Mahkota</td>
+            </tr>
+            <tr>
+                <td>10.00 A.M. TO 9.00 P.M. -Weekend-</td>
+                <td>dercs.ent@gmail.com</td>
+                <td>25000 Kuantan, Pahang</td>
+            </tr>
+            <tr>
+                <td>10.00 A.M. TO 14.00 P.M. -Public Holiday-</td>
+            </tr>
+        </table>
+    </div>
+</body>
+</html>
