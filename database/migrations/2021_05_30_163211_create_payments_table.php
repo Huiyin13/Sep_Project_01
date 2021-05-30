3a822d614d5e66c040b2511ea0c0c9a3843270e0
Teoh <kqteoh@gmail.com>
2021-05-31 00:43:25 +0800

    Create 2021_05_30_163211_create_payments_table.php

---
+++ database/migrations/2021_05_30_163211_create_payments_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePaymentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('payments', function (Blueprint $table) {
+            $table->increments('Payment_ID');
+            $table->string('OrderID');
+            $table->string('Customer_ID');
+            $table->string('Customer_Name');
+            $table->string('Customer_Address');
+            $table->string('Payment_Type');
+            $table->string('Payment_Amount');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('payments');
+    }
+}
